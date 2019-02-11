c726512b1bb048e8a3902d13f77a1642fc527ed2
zhengjim <827301757@qq.com>
2019-02-11 16:22:48 +0800

    test1

--- Run.sh
+++ Run.sh
@@ -2,11 +2,13 @@
 CURRENT_PATH=`dirname $0`
 cd $CURRENT_PATH
 
-XUNFENG_LOG=/var
+XUNFENG_LOG=/var/log/xunfeng
+XUNFENG_DB=/var/lib/mongodb
 
 [ ! -d $XUNFENG_LOG ] && mkdir -p ${XUNFENG_LOG}
+[ ! -d $XUNFENG_DB ] && mkdir -p ${XUNFENG_DB}
 
-
+nohup mongod --port 65521 --dbpath=${XUNFENG_DB} --auth  > ${XUNFENG_LOG}/db.log &
 nohup python ./Run.py > ${XUNFENG_LOG}/web.log &
 nohup python ./aider/Aider.py > ${XUNFENG_LOG}/aider.log &
 nohup python ./nascan/NAScan.py > ${XUNFENG_LOG}/scan.log &
